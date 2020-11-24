aa3db89dc11dc82003839be258095d61abbd31cd
woudie <tieria.dev@gmail.com>
2020-11-24 00:15:36 -0500

    Integrated pin prompt on device linking

--- app/src/main/java/org/thoughtcrime/securesms/DeviceActivity.java
+++ app/src/main/java/org/thoughtcrime/securesms/DeviceActivity.java
@@ -3,31 +3,53 @@ package org.thoughtcrime.securesms;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.transition.TransitionInflater;
+import android.util.DisplayMetrics;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.autofill.HintConstants;
+import androidx.core.app.DialogCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
+import androidx.preference.Preference;
 
+import org.thoughtcrime.securesms.components.SwitchPreferenceCompat;
+import org.thoughtcrime.securesms.contactshare.SimpleTextWatcher;
 import org.thoughtcrime.securesms.crypto.IdentityKeyUtil;
 import org.thoughtcrime.securesms.crypto.ProfileKeyUtil;
 import org.thoughtcrime.securesms.dependencies.ApplicationDependencies;
+import org.thoughtcrime.securesms.keyvalue.PinValues;
+import org.thoughtcrime.securesms.keyvalue.SignalStore;
+import org.thoughtcrime.securesms.lock.PinHashing;
+import org.thoughtcrime.securesms.lock.v2.KbsConstants;
 import org.thoughtcrime.securesms.logging.Log;
 import org.thoughtcrime.securesms.permissions.Permissions;
+import org.thoughtcrime.securesms.pin.PinRestoreActivity;
 import org.thoughtcrime.securesms.qr.ScanListener;
 import org.thoughtcrime.securesms.util.Base64;
 import org.thoughtcrime.securesms.util.DynamicLanguage;
 import org.thoughtcrime.securesms.util.DynamicTheme;
+import org.thoughtcrime.securesms.util.ServiceUtil;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
+import org.thoughtcrime.securesms.util.ThemeUtil;
 import org.thoughtcrime.securesms.util.Util;
 import org.thoughtcrime.securesms.util.task.ProgressDialogAsyncTask;
 import org.whispersystems.libsignal.IdentityKeyPair;
@@ -38,13 +60,14 @@ import org.whispersystems.libsignal.util.guava.Optional;
 import org.whispersystems.signalservice.api.SignalServiceAccountManager;
 import org.whispersystems.signalservice.api.push.exceptions.NotFoundException;
 import org.whispersystems.signalservice.internal.push.DeviceLimitExceededException;
+import org.thoughtcrime.securesms.lock.v2.CreateKbsPinActivity;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class DeviceActivity extends PassphraseRequiredActivity
-    implements Button.OnClickListener, ScanListener, DeviceLinkFragment.LinkClickedListener
+        implements Button.OnClickListener, ScanListener, DeviceLinkFragment.LinkClickedListener
 {
-
   private static final String TAG = DeviceActivity.class.getSimpleName();
 
   private final DynamicTheme    dynamicTheme    = new DynamicTheme();
@@ -105,20 +128,87 @@ public class DeviceActivity extends PassphraseRequiredActivity
     return false;
   }
 
+  private Intent getRoutedIntent(Class<?> destination, @Nullable Intent nextIntent) {
+    final Intent intent = new Intent(this, destination);
+    if (nextIntent != null)   intent.putExtra("next_intent", nextIntent);
+    return intent;
+  }
+
+  private final Intent getPromptPassphraseIntent() {
+    return getRoutedIntent(PassphrasePromptActivity.class, getIntent());
+  }
+
+
   @Override
   public void onClick(View v) {
-    Permissions.with(this)
-               .request(Manifest.permission.CAMERA)
-               .ifNecessary()
-               .withPermanentDenialDialog(getString(R.string.DeviceActivity_signal_needs_the_camera_permission_in_order_to_scan_a_qr_code))
-               .onAllGranted(() -> {
-                 getSupportFragmentManager().beginTransaction()
-                                            .replace(android.R.id.content, deviceAddFragment)
-                                            .addToBackStack(null)
-                                            .commitAllowingStateLoss();
-               })
-               .onAnyDenied(() -> Toast.makeText(this, R.string.DeviceActivity_unable_to_scan_a_qr_code_without_the_camera_permission, Toast.LENGTH_LONG).show())
-               .execute();
+
+    DisplayMetrics metrics = this.getResources().getDisplayMetrics();
+    AlertDialog dialog  = new AlertDialog.Builder(this, ThemeUtil.isDarkTheme(this) ? R.style.Theme_Signal_AlertDialog_Dark_Cornered_ColoredAccent : R.style.Theme_Signal_AlertDialog_Light_Cornered_ColoredAccent)
+            .setView(R.layout.device_add_dialog)
+            .create();
+
+    dialog.show();
+    dialog.getWindow().setLayout((int)(metrics.widthPixels * .80), ViewGroup.LayoutParams.WRAP_CONTENT);
+
+    EditText pinEditText   = (EditText) DialogCompat.requireViewById(dialog, R.id.reminder_disable_pin);
+    TextView statusText    = (TextView) DialogCompat.requireViewById(dialog, R.id.reminder_disable_status);
+    View     cancelButton  = DialogCompat.requireViewById(dialog, R.id.reminder_disable_cancel);
+    View     turnOffButton = DialogCompat.requireViewById(dialog, R.id.reminder_disable_turn_off);
+
+    pinEditText.post(() -> {
+      if (pinEditText.requestFocus()) {
+        ServiceUtil.getInputMethodManager(pinEditText.getContext()).showSoftInput(pinEditText, 0);
+      }
+    });
+
+    ViewCompat.setAutofillHints(pinEditText, HintConstants.AUTOFILL_HINT_PASSWORD);
+
+    switch (SignalStore.pinValues().getKeyboardType()) {
+      case NUMERIC:
+        pinEditText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        break;
+      case ALPHA_NUMERIC:
+        pinEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        break;
+      default:
+        throw new AssertionError("Unexpected type!");
+    }
+
+    pinEditText.addTextChangedListener(new SimpleTextWatcher() {
+      @Override
+      public void onTextChanged(String text) {
+        turnOffButton.setEnabled(text.length() >= KbsConstants.MINIMUM_PIN_LENGTH);
+      }
+    });
+
+    pinEditText.setTypeface(Typeface.DEFAULT);
+
+    turnOffButton.setOnClickListener(nV -> {
+      String  pin     = pinEditText.getText().toString();
+      boolean correct = PinHashing.verifyLocalPinHash(Objects.requireNonNull(SignalStore.kbsValues().getLocalPinHash()), pin);
+
+      if (correct) {
+        Permissions.with(this)
+                .request(Manifest.permission.CAMERA)
+                .ifNecessary()
+                .withPermanentDenialDialog(getString(R.string.DeviceActivity_signal_needs_the_camera_permission_in_order_to_scan_a_qr_code))
+                .onAllGranted(() -> {
+                  getSupportFragmentManager().beginTransaction()
+                          .replace(android.R.id.content, deviceAddFragment)
+                          .addToBackStack(null)
+                          .commitAllowingStateLoss();
+                })
+                .onAnyDenied(() -> Toast.makeText(this, R.string.DeviceActivity_unable_to_scan_a_qr_code_without_the_camera_permission, Toast.LENGTH_LONG).show())
+                .execute();
+        dialog.dismiss();
+      } else {
+
+        statusText.setText(R.string.preferences_app_protection__incorrect_pin_try_again);
+      }
+    });
+
+    cancelButton.setOnClickListener(nV -> dialog.dismiss());
+
   }
 
   @Override
@@ -136,18 +226,18 @@ public class DeviceActivity extends PassphraseRequiredActivity
         deviceLinkFragment.setEnterTransition(TransitionInflater.from(DeviceActivity.this).inflateTransition(android.R.transition.fade));
 
         getSupportFragmentManager().beginTransaction()
-                                   .addToBackStack(null)
-                                   .addSharedElement(deviceAddFragment.getDevicesImage(), "devices")
-                                   .replace(android.R.id.content, deviceLinkFragment)
-                                   .commit();
+                .addToBackStack(null)
+                .addSharedElement(deviceAddFragment.getDevicesImage(), "devices")
+                .replace(android.R.id.content, deviceLinkFragment)
+                .commit();
 
       } else {
         getSupportFragmentManager().beginTransaction()
-                                   .setCustomAnimations(R.anim.slide_from_bottom, R.anim.slide_to_bottom,
-                                                        R.anim.slide_from_bottom, R.anim.slide_to_bottom)
-                                   .replace(android.R.id.content, deviceLinkFragment)
-                                   .addToBackStack(null)
-                                   .commit();
+                .setCustomAnimations(R.anim.slide_from_bottom, R.anim.slide_to_bottom,
+                        R.anim.slide_from_bottom, R.anim.slide_to_bottom)
+                .replace(android.R.id.content, deviceLinkFragment)
+                .addToBackStack(null)
+                .commit();
       }
     });
   }
@@ -161,8 +251,8 @@ public class DeviceActivity extends PassphraseRequiredActivity
   @Override
   public void onLink(final Uri uri) {
     new ProgressDialogAsyncTask<Void, Void, Integer>(this,
-                                                     R.string.DeviceProvisioningActivity_content_progress_title,
-                                                     R.string.DeviceProvisioningActivity_content_progress_content)
+            R.string.DeviceProvisioningActivity_content_progress_title,
+            R.string.DeviceProvisioningActivity_content_progress_content)
     {
       private static final int SUCCESS        = 0;
       private static final int NO_DEVICE      = 1;
