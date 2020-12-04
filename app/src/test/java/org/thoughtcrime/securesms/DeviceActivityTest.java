8e0ecd1686f98960d5823ee8349c861c49825223
Tony Habib <habibt@uoguelph.ca>
2020-12-03 20:13:39 -0500

    Empty DeviceActivityTest.java test suite

---
+++ app/src/test/java/org/thoughtcrime/securesms/DeviceActivityTest.java
@@ -0,0 +1,47 @@
+package org.thoughtcrime.securesms;
+
+import android.view.MenuItem;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import java.awt.*;
+import org.junit.Test;
+
+public class DeviceActivityTest extends TestCase {
+
+    @Test
+    public void testOnPreCreate() throws Exception {
+    }
+
+    @Test
+    public void testOnCreate() throws Exception {
+    }
+
+    @Test
+    public void testOnPause() throws Exception {
+    }
+
+    @Test
+    public void testOnResume() throws Exception {
+    }
+
+    @Test
+    public void testOnOptionsItemSelected() throws Exception {
+
+    }
+
+    @Test
+    public void testOnClick() throws Exception {
+    }
+
+    @Test
+    public void testOnQrDataFound() throws Exception {
+    }
+
+    @Test
+    public void testOnRequestPermissionsResult() throws Exception {
+    }
+
+    @Test
+    public void testOnLink() throws Exception {
+    }
+}
